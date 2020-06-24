5a91f8c7801ecf3385e716c2173c0fb067c91e59
Vishnu Varthan <vishnulatha006@gmail.com>
2020-06-24 15:17:39 +0530

    Completed Formatting badge 🔥 🔥 🔥

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,14 +1,14 @@
-export function getTimeasString(seconds:number,mins:number,hours:number)
-{
-    if(hours > 0){
-        return `${hours.toString()} hr`
-    }
-    else if(mins >0 ){
-        return `${mins.toString()} m `;
-
-    }
-    else if( seconds > 0){
-        return `${seconds.toString()} s`
-    }
-
+export function getTimeasString(seconds: number, mins: number, hours: number) {
+  if (hours > 0) {
+    return `${normalize(hours)} hr`;
+  } else if (mins > 0) {
+    return `${normalize(mins)} m `;
+  } else if (seconds > 0) {
+    return `${normalize(seconds)} s`;
+  }
+}
+function normalize(num: number) {
+  let n = Math.floor(num);
+  let renderString = n.toString();
+  return n;
 }
