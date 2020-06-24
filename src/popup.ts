021e9af446d39536d71d101a706411d0404302c7
Vishnu Varthan <vishnulatha006@gmail.com>
2020-06-24 22:13:07 +0530

    Added Rendering Chart

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,5 @@
 import { Problem, Action } from "./types";
-
+import { Chart } from "chart.js";
 //Dev Stuffs
 // @ts-ignore
 const console = chrome.extension.getBackgroundPage().console;
@@ -10,15 +10,36 @@ const problemTitleDiv = document.querySelector(
 const difficultyDiv = document.querySelector(
   "#difficulty-div"
 ) as HTMLSpanElement;
+
+// Buttons Div
+
 const startBtn = document.getElementById("start") as HTMLButtonElement;
 const resetBtn = document.getElementById("reset") as HTMLButtonElement;
+const saveBtn = document.getElementById("save") as HTMLButtonElement;
 // Get Title of the Current Page and URL
 const backgroundPage = chrome.extension.getBackgroundPage();
 
 //  Background Page
 
+//@ts-ignore
+startBtn.addEventListener("click", function () {
+  //@ts-ignore
+  backgroundPage.startStop();
+});
+
+resetBtn.addEventListener("click", function () {
+  //@ts-ignore
+  backgroundPage.resetFunc();
+});
+
+saveBtn.addEventListener("click", function () {
+  //@ts-ignore
+  backgroundPage.saveData();
+});
 
 renderTimerPage();
+
+document.addEventListener("DOMContentLoaded", renderChart);
 
 function renderTimerPage() {
   let problemDict: Problem = {};
@@ -49,12 +70,38 @@ function createElement(): HTMLElement {
 }
 
 //@ts-ignore
-startBtn.addEventListener("click", function () {
-  //@ts-ignore
-  backgroundPage.startStop();
-});
 
-resetBtn.addEventListener("click", function () {
-  //@ts-ignore
-  backgroundPage.resetFunc();
-});
+function renderChart() {
+  let easyCount = 0;
+  let mediumCount = 0;
+  let hardCount = 0;
+ 
+const items  = localStorage.getItem("leetCodeExtensionDetails");
+if(items){
+const parsedItem = JSON.parse(items);
+easyCount = parsedItem.easy.length;
+mediumCount = parsedItem.medium.length;
+hardCount = parsedItem.hard.length
+
+}
+ //@ts-ignore
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      labels: ["Easy", "Medium", "Hard"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [easyCount,mediumCount,hardCount],
+          backgroundColor: [
+            "rgb(68,160,72)",
+            "rgb(239,113,9)",
+            "rgb(233,32,99)",
+          ],
+        },
+      ],
+    },
+    options: {},
+  });
+}
