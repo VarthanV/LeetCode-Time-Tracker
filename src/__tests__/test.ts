a81404cc968ab73c78cf0514812bd73083145374
Vishnu Varthan <vishnulatha006@gmail.com>
2020-06-25 12:45:48 +0530

    Added Tests

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -3,15 +3,17 @@ import {getTimeasString} from '../helpers';
 import { normalize } from '../helpers';
 
 
-test('Returns 2m' ,() =>{
+test('Returns 2m ' ,() =>{
     expect(getTimeasString(0,2,0)).toBe('2m')
 })
-test('Returns 3'  , () =>{
+test('Returns 3 '  , () =>{
     
     expect(normalize(0o3)).toBe(3)
 })
 
-test('Returns 1hr 25m' , () =>{
+test('Returns 1hr 25min ' , () =>{
     expect(getTimeasString(0,25,1)).toBe('1hr 25min')
 })
+
+
 
